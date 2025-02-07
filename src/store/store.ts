42c5ef839fbc62d87a8f4ce75e127a46a8d720b1
Konstantin Slastikhin <Apple.Xrum@yandex.ru>
2025-02-07 11:59:13 +0300

    Правка по импорту в store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "@store/api/api.ts";
-import { balanceSlice } from "@store/balance/slice/balance.slice.ts";
-import { botsSlice } from "@store/bots/slice/bots.slice.ts";
-import { periodSlice } from "@store/period/slice/period.slice.ts";
+import balanceReducer from "@store/balance/slice/balance.slice.ts";
+import botsReducer from "@store/bots/slice/bots.slice.ts";
+import periodReducer from "@store/period/slice/period.slice.ts";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    balance: balanceSlice.reducer,
-    bots: botsSlice.reducer,
-    period: periodSlice.reducer,
+    balance: balanceReducer,
+    bots: botsReducer,
+    period: periodReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
 });
